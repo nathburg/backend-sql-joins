bbc17b0f6581e2eb1087780aa9fba4ea289a9361
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-10 17:38:54 -0800

    all tests pass

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,14 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+SELECT
+	film.title,
+    film.description,
+    film.release_year,
+    COALESCE(json_agg(to_jsonb(actor))) as actors
+FROM film
+LEFT JOIN film_actor
+ON film_actor.film_id = film.film_id
+LEFT JOIN actor
+ON actor.actor_id = film_actor.actor_id
+WHERE film.title = 'Airplane Sierra'
+GROUP BY film.film_id
