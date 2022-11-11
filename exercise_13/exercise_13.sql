0ead79719a6d3772a8eb02563a98001c8433b307
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-10 17:07:58 -0800

    13th test passes

--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,16 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+SELECT AVG(payment.amount), country.country
+FROM payment
+	LEFT JOIN customer
+    ON customer.customer_id = payment.customer_id
+    LEFT JOIN address
+    ON address.address_id = customer.address_id
+    LEFT JOIN city
+    ON city.city_id = address.city_id
+    LEFT JOIN country
+    ON country.country_id = city.country_id
+GROUP BY country.country_id
+ORDER BY AVG(payment.amount) DESC
+LIMIT 10
